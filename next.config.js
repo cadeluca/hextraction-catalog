06cbcd390844c02f927b1ef847dbe4cbbbe5d0f0
Christian DeLuca <32560733+cadeluca@users.noreply.github.com>
2024-10-28 00:17:43 -0400

    update next config for svgs, add svgs, and tweak jsconfig to correct import issues

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
-import nextra from 'nextra'
+import path from "node:path";
+import nextra from "nextra";
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-})
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
 
 // const isProduction = process.env.NODE_ENV === "production";
 // const assetPrefix = isProduction ? "/hextraction-catalog" : "";
-const assetPrefix = "/hextraction-catalog"
+const assetPrefix = "/hextraction-catalog";
+const sep = path.sep === "/" ? "/" : "\\\\";
+const ALLOWED_SVG_REGEX = new RegExp(`src${sep}components${sep}icons${sep}.+\\.svg$`);
 
 const nextConfig = {
   images: {
@@ -20,7 +23,18 @@ const nextConfig = {
   output: "export",
 };
 
-export default {
-  ...withNextra(),
+export default withNextra({
   ...nextConfig,
-}
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+    fileLoaderRule.exclude = ALLOWED_SVG_REGEX;
+
+    config.module.rules.push({
+      test: ALLOWED_SVG_REGEX,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});
